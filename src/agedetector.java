8e687c45fef55c5e4d5f1b3da71415fbf6a593d8
vamsi krishna Narra <vamsin.ar97@gmail.com>
2023-06-06 14:55:29 -0500

    Initial commit

---
+++ src/agedetector.java
@@ -0,0 +1,17 @@
+public class agedetector {
+    public static void main(String[] args) {
+        int age = 19; // Example age
+
+        classifyAge(age);
+    }
+
+    public static void classifyAge(int age) {
+        if (age < 13) {
+            System.out.println("Kid");
+        } else if (age <= 19) {
+            System.out.println("Teen");
+        } else {
+            System.out.println("Adult");
+        }
+    }
+}
